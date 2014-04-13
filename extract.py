f0858808eb643b96af79deedb506b90e76906abe
Steve Bennett <stevage@gmail.com>
2014-04-13 23:46:35 +1000

    0.0

---
+++ extract.py
@@ -0,0 +1,13 @@
+# Extracts bike paths from OSM data in PostGIS, saves as GeoJSON.
+# Written by Steve Bennett for OKFNau, released under WTFPL.
+from psycopg2 import *
+from subprocess import call
+
+conn = connect(dbname='gis')
+cur=conn.cursor()
+
+cur.execute("SELECT distinct osm_id,route_name from planet_osm_line where route_name like '%Rail Trail' and network='rcn';")
+
+for record in cur:
+  print "%s" % record[1]
+  call (["ogr2ogr", "-f", "GeoJSON", record[1] + ".json",  'PG:dbname=\'gis\'', "-sql", 'SELECT * from planet_osm_line where osm_id=%d' % record[0]])
