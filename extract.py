d806a100af806177b3acecbb1849887ee352c58b
Steve Bennett <stevage@gmail.com>
2014-04-15 15:12:46 +1000

    Try explicit projection from EPSG 3857

--- extract.py
+++ extract.py
@@ -6,8 +6,8 @@ from subprocess import call
 conn = connect(dbname='gis')
 cur=conn.cursor()
 
-cur.execute("SELECT distinct osm_id,route_name from planet_osm_line where route_name is not null and network='rcn';")
+cur.execute("SELECT distinct osm_id,route_name from planet_osm_line where route_name like 'Yuroke%' and network='rcn';")
 
 for record in cur:
   print "%s" % record[1]
-  call (["ogr2ogr", "-f", "GeoJSON", record[1] + ".json",  'PG:dbname=\'gis\'', "-sql", 'SELECT route_name,osm_id,tags::hstore->\'state\' as state,way from planet_osm_line where osm_id=%d' % record[0], "-t_srs", "EPSG: 4326"])
+  call (["ogr2ogr", "-f", "GeoJSON", record[1] + ".json",  'PG:dbname=\'gis\'', "-sql", 'SELECT route_name,osm_id,tags::hstore->\'state\' as state,way from planet_osm_line where osm_id=%d' % record[0], "-t_srs", "EPSG:4326", "-s_srs", "EPSG:3857"])
