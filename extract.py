1bdf4df99c182e04bc86f2ef5db182fa4e5b67f4
Steve Bennett <stevage@gmail.com>
2014-04-15 15:30:11 +1000

    Include LCN, NCN, RCN

--- extract.py
+++ extract.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python
 # Extracts bike paths from OSM data in PostGIS, saves as GeoJSON.
 # Written by Steve Bennett for OKFNau, released under WTFPL.
 from psycopg2 import *
@@ -5,9 +6,10 @@ from subprocess import call
 
 conn = connect(dbname='gis')
 cur=conn.cursor()
+networks = ['lcn','rcn','ncn']
+for network in networks:
+  cur.execute("SELECT distinct osm_id,route_name from planet_osm_line where route_name is not null and network='%s';" % network)
 
-cur.execute("SELECT distinct osm_id,route_name from planet_osm_line where route_name is not null and network='rcn';")
-
-for record in cur:
-  print "%s" % record[1]
-  call (["ogr2ogr", "-f", "GeoJSON", record[1] + ".json",  'PG:dbname=\'gis\'', "-sql", 'SELECT route_name,osm_id,tags::hstore->\'state\' as state,way from planet_osm_line where osm_id=%d' % record[0], "-t_srs", "EPSG:4326", "-s_srs", "EPSG:3857"])
+  for record in cur:
+    print "%s" % record[1]
+    call (["ogr2ogr", "-f", "GeoJSON", network + "/" + record[1] + ".json",  'PG:dbname=\'gis\'', "-sql", 'SELECT route_name,osm_id,tags::hstore->\'state\' as state,way from planet_osm_line where osm_id=%d' % record[0], "-t_srs", "EPSG:4326", "-s_srs", "EPSG:3857"])
